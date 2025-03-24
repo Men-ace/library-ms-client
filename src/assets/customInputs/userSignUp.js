285782aa74065677f02e56bb8ce19cd1227e3562
Shailesh Gharti <124006792+Men-ace@users.noreply.github.com>
2025-03-24 12:28:29 +1100

    created useFrom hook

---
+++ src/assets/customInputs/userSignUp.js
@@ -0,0 +1,51 @@
+
+
+ export const signUpInputs = [
+    {
+        label: "First Name*",  
+        name: "fName", 
+        type: "text", 
+        required: true, 
+        placeholder: "John", 
+      
+    },
+    {
+        label: "Last Name*",  
+        name: "lName", 
+        type: "text", 
+        required: true, 
+        placeholder: "Smith", 
+      
+    },
+    {
+        label: "Email*",  
+        name: "email", 
+        type: "email", 
+        required: true, 
+        placeholder: "Smith@123gmail.com", 
+      
+    },
+    {
+        label: "phone",  
+        name: "phone", 
+        type: "number", 
+        placeholder: "+61 400000000", 
+      
+    },
+    {
+        label: "Password*",  
+        name: "password", 
+        type: "password", 
+        required: true, 
+        placeholder: "*********", 
+      
+    },
+    {
+        label: "Confirm Password*",  
+        name: "confirmPassword", 
+        type: "password", 
+        required: true, 
+        placeholder: "********", 
+      
+    },
+]
--- src/pages/auth/SignUpPage.jsx
+++ src/pages/auth/SignUpPage.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { CustomInput } from '../../components/customInput/CustomInput';
+import { signUpInputs } from '../../assets/customInputs/userSignUp';
+
 
 const SignUpPage = () => {
   return (
-    <div>
-      signup
+    <div className='d-flex justify-content-center'>
+     <Form style={{width:"450px"}} className="card p-3 shadow-lg mt-5 mb-5">
+      <h1>Join Our Library Community</h1>
+      <hr />
+      {
+        signUpInputs.map((inputs)=> <CustomInput keys={inputs.name} {...inputs}/> 
+
+        )
+      }
+
+      <Button variant="primary" type="submit" className='mb-4'>
+        Submit
+      </Button>
+    </Form>
     </div>
   )
 }
