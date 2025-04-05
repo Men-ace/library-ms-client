e419d2c0114b68ad2798ac4e2ea1d2c61337b498
Shailesh Gharti <124006792+Men-ace@users.noreply.github.com>
2025-04-05 21:00:05 +1100

    made section folder to put title

--- src/components/layouts/UserLayout.jsx
+++ src/components/layouts/UserLayout.jsx
@@ -1,32 +1,4 @@
-import React from 'react'
-import { Header } from './Header'
-import Footer   from './Footer'
-import { Outlet } from 'react-router-dom'
-import { Container, Row, Col } from 'react-bootstrap'
-import { Sidebar } from './SideBar'
-import { AuthRoute } from '../auth/AuthRoute'
-
-
-const UserLayout = () => {
-  return (
-   <AuthRoute>
-      {/* navbar */}
-        <Header />
-
-
-    <Container fluid>
-      <Row>
-        <Col xs={2} md={3} xl={2}className='bg-dark text-white'>
-            <div className="p3">
-              <div>welcome back</div>
-              <h4>shailesh</h4>
-            </div>
-            <hr />
-            <Sidebar />
-
-
-        </Col>
-        import React from "react";
+import React from "react";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { Outlet } from "react-router-dom";
@@ -44,7 +16,7 @@ export const UserLayout = () => {
         <div className="bg-dark text-white p-3 " style={{ width: "200px" }}>
           <div className="p-3">
             <div>Welcome Back</div>
-            <h4>Shailesh Gharti</h4>
+            <h4>Prem Acharya</h4>
           </div>
           <hr />
           <Sidebar />
@@ -61,23 +33,3 @@ export const UserLayout = () => {
     </AuthRoute>
   );
 };
-
-        <Col xs={10} md={9}  xl={10}>
-          {/* main body */}
-              <main className='main'>
-                  <Outlet/>
-              </main>
-      </Col>
-
-      </Row>
-    </Container>
-
-
-    {/* footer */}
-        <Footer />
-        
-   </AuthRoute>
-  )
-}
-
-export default UserLayout
